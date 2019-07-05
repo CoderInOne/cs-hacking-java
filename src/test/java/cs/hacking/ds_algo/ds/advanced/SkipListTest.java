f60220c6412b7429eb02e4c1d359091728d27a89
lihuangdong <lihuangdong@mozat.com>
2019-07-05 11:02:48 +0800

    skip list DONE

--- src/test/java/cs/hacking/ds_algo/ds/advanced/SkipListTest.java
+++ src/test/java/cs/hacking/ds_algo/ds/advanced/SkipListTest.java
@@ -3,14 +3,12 @@ package cs.hacking.ds_algo.ds.advanced;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 public class SkipListTest {
     private SkipList skipList;
 
     @Before
     public void setUp() throws Exception {
-        skipList = new SkipList();
+        skipList = new SkipList(0.5F);
     }
 
     @Test
@@ -18,5 +16,38 @@ public class SkipListTest {
         skipList.add(1);
         skipList.add(2);
         skipList.add(3);
+
+        System.out.println(skipList);
+
+        System.out.println(skipList.search(1));
+        System.out.println(skipList.search(2));
+        System.out.println(skipList.search(3));
+    }
+
+    @Test
+    public void delete() {
+        skipList.add(1);
+        skipList.add(2);
+        skipList.add(3);
+        skipList.add(4);
+        skipList.add(5);
+
+        System.out.println(skipList);
+
+        System.out.println(skipList.search(4));
+
+        skipList.delete(4);
+
+        System.out.println(skipList.search(4));
+
+        System.out.println(skipList);
+    }
+
+    @Test
+    public void lvl() {
+        skipList.setCurLevel(3);
+        for (int i = 0; i < 10; i++) {
+            System.out.println(skipList.randomLevel());
+        }
     }
 }
