7b16c335614b4a8eef9849a41e5fc8e7d70bd6a8
lihuangdong@mozat.com <lihuangdong@mozat.com>
2019-07-07 00:18:38 +0800

    string matching: robin karp

--- src/main/java/cs/hacking/ds_algo/strings/StringMatcher.java
+++ src/main/java/cs/hacking/ds_algo/strings/StringMatcher.java
@@ -14,7 +14,7 @@ public class StringMatcher {
 		while (i <= s.length() - p.length()) {
 			if (s.charAt(i) == p.charAt(0)) {
 				for (int j = 1; j < p.length(); j++) {
-					if (s.charAt(i+j) != p.charAt(j)) {
+					if (s.charAt(i + j) != p.charAt(j)) {
 						i += j;
 						break;
 					}
@@ -24,11 +24,43 @@ public class StringMatcher {
 						i += p.length();
 					}
 				}
-			}
-			else {
+			} else {
 				i++;
 			}
 		}
+		return res;
+	}
+
+	public List<Integer> robenKarp(String s, String p, int d, int q) {
+		List<Integer> res = new ArrayList<>();
+		int hp = 0;
+		int hs = 0;
+		int h = d;
+
+		for (int i = 0; i < p.length() - 1; i++) {
+			h = (h * d) % q;
+		}
+
+		// pre process
+		for (int j = 0; j < p.length(); j++) {
+			hp = (hp * d + p.charAt(j)) % q;
+			hs = (hs * d + s.charAt(j)) % q;
+		}
+
+		for (int k = 0; k <= s.length() - p.length(); k++) {
+			if (hs == hp) {
+				for (int t = 0; t < p.length(); t++) {
+					if (s.charAt(t + k) != p.charAt(t)) {
+						break;
+					} else if (t == p.length() - 1) {
+						res.add(k);
+					}
+				}
+			} else if (k + 1 <= s.length() - p.length()) {
+				hs = (d * (hs - s.charAt(k) * h + s.charAt(k + 1))) % q;
+			}
+		}
+
 		return res;
 	}
 }
--- src/test/java/cs/hacking/ds_algo/strings/StringMatchTest.java
+++ src/test/java/cs/hacking/ds_algo/strings/StringMatchTest.java
@@ -34,4 +34,22 @@ public class StringMatchTest {
 					e.getValue().toArray(), res.toArray());
 		}
 	}
+
+	@Test
+	public void robinKarp() {
+		Map<String, List<Integer>> testMap = new LinkedHashMap<>();
+		String pattern = "1234";
+		testMap.put("1234", Collections.singletonList(0));
+		testMap.put("1234123", Collections.singletonList(0));
+		testMap.put("12123412", Collections.singletonList(2));
+		testMap.put("121234123422", Arrays.asList(2, 6));
+		testMap.put("1212344123422", Arrays.asList(2, 7));
+
+		for (Map.Entry<String, List<Integer>> e : testMap.entrySet()) {
+			List<Integer> res = strMatcher.robenKarp(e.getKey(), pattern, 10, 11);
+			assertArrayEquals(String.format("fail input:%s, output:%s, expect:%s",
+					e.getKey(), res, e.getValue()),
+					e.getValue().toArray(), res.toArray());
+		}
+	}
 }
