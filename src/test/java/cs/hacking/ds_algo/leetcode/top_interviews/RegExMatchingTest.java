1855dc9ac90bdb51c85650d7eb31cc01ae643355
lihuangdong <lihuangdong@mozat.com>
2019-07-08 14:17:03 +0800

    regex matching FAIL

--- src/test/java/cs/hacking/ds_algo/leetcode/top_interviews/RegExMatchingTest.java
+++ src/test/java/cs/hacking/ds_algo/leetcode/top_interviews/RegExMatchingTest.java
@@ -17,9 +17,14 @@ public class RegExMatchingTest {
         assertTrue(matcher.match("aaa", "aaa*"));
         assertTrue(matcher.match("ab", ".*"));
         assertTrue(matcher.match("aba", ".*"));
+        assertTrue(matcher.match("aab", "a*a*b"));
         assertTrue(matcher.match("aab", "c*a*b"));
+        assertTrue(matcher.match("a", "ab*b*"));
+        assertTrue(matcher.match("", "a*"));
 
         assertFalse(matcher.match("aa", "a"));
+        assertFalse(matcher.match("aaa", "aaaa"));
         assertFalse(matcher.match("mississippi", "mis*is*ip*"));
+        assertFalse(matcher.match("abcd", "d*"));
     }
 }
