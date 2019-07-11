49e57f35eca847ae033ec4e3bed7e50697736460
lihuangdong <lihuangdong@mozat.com>
2019-07-11 10:22:52 +0800

    valid parenthese done

---
+++ src/test/java/cs/hacking/ds_algo/leetcode/top_interviews/ValidParenthesesTest.java
@@ -0,0 +1,31 @@
+package cs.hacking.ds_algo.leetcode.top_interviews;
+
+import cs.hacking.ds_algo.Pair;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
+public class ValidParenthesesTest {
+
+    @Test
+    public void isValid() {
+        List<Pair<String, Boolean>> list = new ArrayList<>();
+        list.add(Pair.from("((()))", true));
+        list.add(Pair.from("(()())", true));
+        list.add(Pair.from("()()()", true));
+        list.add(Pair.from("()()(", false));
+        list.add(Pair.from(")()", false));
+        list.add(Pair.from("((", false));
+
+        ValidParentheses validParentheses = new ValidParentheses();
+
+        for (Pair<String, Boolean> p : list) {
+            assertEquals("fail for " + p.getOne(),
+                    p.getTwo(), validParentheses.isValid(p.getOne()));
+        }
+    }
+}
