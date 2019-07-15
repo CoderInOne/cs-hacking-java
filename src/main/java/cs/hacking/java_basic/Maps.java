0a0f5e174b9997e35e9bd868c6e980999cced43f
lihuangdong@mozat.com <lihuangdong@mozat.com>
2019-07-15 22:24:00 +0800

    Map iteration

---
+++ src/main/java/cs/hacking/java_basic/Maps.java
@@ -0,0 +1,22 @@
+package cs.hacking.java_basic;
+
+public class Maps {
+	/** iteration
+	 *      System.out.println("entry set");
+	 * 		for (Map.Entry<Integer, Integer> e : map.entrySet()) {
+	 * 			System.out.println(e.getValue());
+	 *                }
+	 *
+	 *      // can remove when iterating
+	 * 		System.out.println("iterator");
+	 * 		Iterator<Map.Entry<Integer, Integer>> iterator = map.entrySet().iterator();
+	 * 		while (iterator.hasNext()) {
+	 * 			System.out.println(iterator.next().getValue());
+	 *        }
+	 *
+	 * 		System.out.println("java8 foreach");
+	 * 		map.forEach((k, v) -> {
+	 * 			System.out.println(v);
+	 *      });
+	 */
+}
